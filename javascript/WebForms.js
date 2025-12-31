1132cc92d8161d9ecec87ee4ab087d8142a598e0
Elanat Framework <111444759+elanatframework@users.noreply.github.com>
2026-01-01 00:32:07 +0330

    Update WebForms.js

--- javascript/WebForms.js
+++ javascript/WebForms.js
@@ -1858,15 +1858,15 @@ class InputPlace {
         return `<${tag}>*`;
     }
 
-    static className(className) {
+    static cssClass(className) {
         return `{${className}}`;
     }
 
-    static classNameWithIndex(className, index) {
+    static cssClassWithIndex(className, index) {
         return `{${className}}${index}`;
     }
 
-    static allClasses(className) {
+    static allCssClasses(className) {
         return `{${className}}*`;
     }
 
