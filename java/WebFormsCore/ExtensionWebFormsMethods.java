d8ae3e4457a41e27f7529c5406089c2d3015a42c
Elanat Framework <111444759+elanatframework@users.noreply.github.com>
2026-01-02 02:47:50 +0330

    Update ExtensionWebFormsMethods.java

--- java/WebFormsCore/ExtensionWebFormsMethods.java
+++ java/WebFormsCore/ExtensionWebFormsMethods.java
@@ -1,13 +1,14 @@
 package WebFormsCore;
 
-// Compatible with WebFormsJS version 1.6
+// WebForms.java 2.0 - The Back-End Part of WebForms Core Technology, Owned by Elanat (https://elanat.net)
+// Compatible with WebFormsJS version 2.0
 
 public class ExtensionWebFormsMethods {
     /// <summary>
     /// This Method Does Not Support QueryAll
     /// </summary>
     public static String appendPlace(String text, String value) {
-        if (text.length() < 1)
+        if (text == null || text.length() < 1)
             return value;
 
         return text + "|" + value;
@@ -17,17 +18,24 @@ public class ExtensionWebFormsMethods {
         return "/" + text;
     }
 
-    public static String exportToWebFormsTag(String src) {
-        return "<web-forms src=\"" + src + "\"></web-forms>";
-    }
-
-    // Overload
-    public static String exportToWebFormsTag(String src, int width, int height) {
-        return "<web-forms src=\"" + src + "\" width=\"" + width + "\" height=\"" + height + "\"></web-forms>";
+    public static String exportActionControlsToWebFormsTag(String actionControls, boolean addLine) {
+        return (addLine ? "\n" : "") + "<web-forms ac=\"" + actionControls + "\"></web-forms>";
     }
 
     public static String exportActionControlsToWebFormsTag(String actionControls) {
-        return "<web-forms ac=\"" + actionControls + "\"></web-forms>";
+        return exportActionControlsToWebFormsTag(actionControls, false);
+    }
+
+    public static String exportActionControlsToHtmlComment(String actionControls, boolean addLine) {
+        return (addLine ? "\n" : "") + "<!--[web-forms]\n" + actionControls + "-->";
+    }
+
+    public static String exportActionControlsToHtmlComment(String actionControls) {
+        return exportActionControlsToHtmlComment(actionControls, false);
+    }
+
+    public static String exportActionControlsToResponse(String actionControls) {
+        return "[web-forms]\n" + actionControls;
     }
 
     public static String removeOuter(String text, String startString, String endString) {
@@ -42,5 +50,9 @@ public class ExtensionWebFormsMethods {
         int lengthToRemove = (end - start) + endString.length();
 
         return text.substring(0, start) + text.substring(start + lengthToRemove);
+    }
+
+    public static String lineBreak(String text) {
+        return text.replace("\n", "$[sln]");
     }
 }
